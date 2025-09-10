daf7c6c76f2d468355b57dc1711ed5ebcf18be80
DANR1BEIRO <danribeirodev@gmail.com>
2025-09-10 13:13:46 -0300

    feat: Implementa a estrutura completa para o recurso de Categoria.
    - Cria a entidade Category com ID e nome.
    - Adiciona o CategoryRepository para acesso aos dados.
    - Implementa o CategoryService com a lógica de busca.
    - Expõe um endpoint GET /movieflix/category para listar todas as categorias.

---
+++ src/main/java/br/com/movieflix/movieflix/service/CategoryService.java
@@ -0,0 +1,19 @@
+package br.com.movieflix.movieflix.service;
+
+import br.com.movieflix.movieflix.entity.Category;
+import br.com.movieflix.movieflix.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+}
